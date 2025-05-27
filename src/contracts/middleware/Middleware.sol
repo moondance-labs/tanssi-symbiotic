cc8ab7fcffdd1e0c871b38586910b1cddbe9ac3a
steven2308 <steven2308@outlook.com>
2025-05-27 14:35:31 -0500

    Implements hook for operator specific vaults so vault is linked to collateral.

--- src/contracts/middleware/Middleware.sol
+++ src/contracts/middleware/Middleware.sol
@@ -466,6 +466,14 @@ contract Middleware is
     /**
      * @inheritdoc BaseOperators
      */
+    function _beforeRegisterOperatorVault(address, address vault) internal override {
+        address collateral = IVault(vault).collateral();
+        _setVaultToCollateral(vault, collateral);
+    }
+
+    /**
+     * @inheritdoc BaseOperators
+     */
     function _beforeRegisterOperator(
         address operator,
         bytes memory key,
