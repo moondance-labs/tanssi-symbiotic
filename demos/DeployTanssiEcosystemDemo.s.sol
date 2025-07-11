4f4ad0dfd16e3ca868413b1e87afe1dbbac726f2
NindoK <b.daniele@protonmail.ch>
2025-07-11 17:35:06 +0200

    Fix demo script

--- demos/DeployTanssiEcosystemDemo.s.sol
+++ demos/DeployTanssiEcosystemDemo.s.sol
@@ -36,6 +36,8 @@ import {Subnetwork} from "@symbiotic/contracts/libraries/Subnetwork.sol";
 //**************************************************************************************************
 import {MockV3Aggregator} from "@chainlink/tests/MockV3Aggregator.sol";
 
+import {ODefaultStakerRewardsFactory} from "src/contracts/rewarder/ODefaultStakerRewardsFactory.sol";
+import {ODefaultStakerRewards} from "src/contracts/rewarder/ODefaultStakerRewards.sol";
 import {ODefaultOperatorRewards} from "src/contracts/rewarder/ODefaultOperatorRewards.sol";
 import {IODefaultStakerRewards} from "src/interfaces/rewarder/IODefaultStakerRewards.sol";
 import {Middleware} from "src/contracts/middleware/Middleware.sol";
@@ -82,6 +84,8 @@ contract DeployTanssiEcosystemDemo is Script {
     bytes32 public operatorKey1 = vm.envOr("OPERATOR1_KEY", OPERATOR_KEY);
     bytes32 public operatorKey2 = vm.envOr("OPERATOR2_KEY", OPERATOR_KEY2);
     bytes32 public operatorKey3 = vm.envOr("OPERATOR3_KEY", OPERATOR_KEY3);
+
+    address public stakerRewardsImpl;
 
     bool public isTest = false;
     VaultAddresses public vaultAddresses;
@@ -246,7 +250,8 @@ contract DeployTanssiEcosystemDemo is Script {
             adminFee: 0,
             defaultAdminRoleHolder: tanssi,
             adminFeeClaimRoleHolder: tanssi,
-            adminFeeSetRoleHolder: tanssi
+            adminFeeSetRoleHolder: tanssi,
+            implementationStakerRewards: stakerRewardsImpl
         });
         ecosystemEntities.middleware.registerSharedVault(vaultAddresses.vault, stakerRewardsParams);
         ecosystemEntities.middleware.registerSharedVault(vaultAddresses.vaultVetoed, stakerRewardsParams);
@@ -363,7 +368,7 @@ contract DeployTanssiEcosystemDemo is Script {
         ODefaultOperatorRewards operatorRewards = ODefaultOperatorRewards(operatorRewardsAddress);
 
         address stakerRewardsFactoryAddress = contractScripts.deployRewards.deployStakerRewardsFactoryContract(
-            vaultRegistryAddress, address(networkMiddlewareService), operatorRewardsAddress, tanssi
+            vaultRegistryAddress, address(networkMiddlewareService), operatorRewardsAddress, tanssi, tanssi
         );
 
         vm.startBroadcast(ownerPrivateKey);
@@ -376,11 +381,15 @@ contract DeployTanssiEcosystemDemo is Script {
             owner: tanssi,
             epochDuration: NETWORK_EPOCH_DURATION,
             slashingWindow: SLASHING_WINDOW,
-            reader: address(0)
+            reader: address(0),
+            operatorRewards: operatorRewardsAddress,
+            stakerRewardsFactory: stakerRewardsFactoryAddress
         });
 
-        ecosystemEntities.middleware =
-            _deployMiddlewareWithProxy(params, operatorRewardsAddress, stakerRewardsFactoryAddress);
+        ecosystemEntities.middleware = _deployMiddlewareWithProxy(params);
+
+        stakerRewardsImpl = address(new ODefaultStakerRewards(address(networkMiddlewareService), tanssi));
+        ODefaultStakerRewardsFactory(stakerRewardsFactoryAddress).whitelist(stakerRewardsImpl);
 
         operatorRewards.grantRole(operatorRewards.MIDDLEWARE_ROLE(), address(ecosystemEntities.middleware));
         operatorRewards.grantRole(operatorRewards.STAKER_REWARDS_SETTER_ROLE(), address(ecosystemEntities.middleware));
@@ -437,11 +446,9 @@ contract DeployTanssiEcosystemDemo is Script {
     }
 
     function _deployMiddlewareWithProxy(
-        IMiddleware.InitParams memory params,
-        address operatorRewardsAddress,
-        address stakerRewardsFactoryAddress
+        IMiddleware.InitParams memory params
     ) private returns (Middleware _middleware) {
-        Middleware _middlewareImpl = new Middleware(operatorRewardsAddress, stakerRewardsFactoryAddress);
+        Middleware _middlewareImpl = new Middleware();
         _middleware = Middleware(address(new MiddlewareProxy(address(_middlewareImpl), "")));
         console2.log("Middleware Implementation: ", address(_middlewareImpl));
 
