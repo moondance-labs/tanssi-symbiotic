34c8c1ee89e3161454ae520cb6516c884f37ba19
NindoK <b.daniele@protonmail.ch>
2025-07-15 15:40:27 +0200

    Took out unusued import

--- src/contracts/middleware/MiddlewareStorage.sol
+++ src/contracts/middleware/MiddlewareStorage.sol
@@ -14,8 +14,6 @@
 // along with Tanssi.  If not, see <http://www.gnu.org/licenses/>
 pragma solidity 0.8.25;
 
-import {IMiddleware} from "src/interfaces/middleware/IMiddleware.sol";
-
 abstract contract MiddlewareStorage {
     /// @custom:storage-location erc7201:tanssi.middleware.MiddlewareStorage.v1.1
     struct StorageMiddleware {
