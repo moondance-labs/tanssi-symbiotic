dc9bb56a0181889d943bfc54d6b416e6099e9bdf
NindoK <b.daniele@protonmail.ch>
2025-07-18 02:28:27 +0200

    Fix

--- src/contracts/oracle-proxy/AggregatorV3Proxy.sol
+++ src/contracts/oracle-proxy/AggregatorV3Proxy.sol
@@ -5,16 +5,20 @@ pragma solidity 0.8.25;
 //                                      CHAINLINK
 //**************************************************************************************************
 import {AggregatorV3Interface} from "@chainlink/shared/interfaces/AggregatorV2V3Interface.sol";
-
 import {IDIAOracleV2} from "src/interfaces/oracles/IDIAOracleV2.sol";
 
 contract AggregatorV3Proxy is AggregatorV3Interface {
     string public pairSymbol;
     IDIAOracleV2 public aggregator;
 
+    error AggregatorV3Proxy__InvalidData();
+
     constructor(address _aggregator, string memory _pairSymbol) {
-        pairSymbol = pairSymbol;
+        if (_aggregator == address(0) || bytes(_pairSymbol).length == 0) {
+            revert AggregatorV3Proxy__InvalidData();
+        }
         aggregator = IDIAOracleV2(_aggregator);
+        pairSymbol = _pairSymbol;
     }
 
     function decimals() external pure override returns (uint8) {
@@ -31,12 +35,7 @@ contract AggregatorV3Proxy is AggregatorV3Interface {
 
     function getRoundData(
         uint80 /*_roundId*/
-    )
-        external
-        view
-        override
-        returns (uint80 roundId, int256 ans, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
-    {
+    ) external view override returns (uint80, int256, uint256, uint256, uint80) {
         (uint128 latestPrice, uint128 timestampOfLatestPrice) = aggregator.getValue(pairSymbol);
 
         return (
@@ -48,17 +47,12 @@ contract AggregatorV3Proxy is AggregatorV3Interface {
         );
     }
 
-    function latestRoundData()
-        external
-        view
-        override
-        returns (uint80 roundId, int256 ans, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
-    {
+    function latestRoundData() external view override returns (uint80, int256, uint256, uint256, uint80) {
         (uint128 latestPrice, uint128 timestampOfLatestPrice) = aggregator.getValue(pairSymbol);
 
         return (
             uint80(1),
-            ansint256(uint256(latestPrice)) wer,
+            int256(uint256(latestPrice)),
             uint256(timestampOfLatestPrice),
             uint256(timestampOfLatestPrice),
             uint80(1)
