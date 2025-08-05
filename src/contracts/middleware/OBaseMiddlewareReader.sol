226f25fea0ab9f47bcff5c2911c77a13a1e8c2c2
Steven Pineda <steven2308@outlook.com>
2025-08-05 10:37:16 -0500

    Improves readability and gas on getValidatorSet.

--- src/contracts/middleware/OBaseMiddlewareReader.sol
+++ src/contracts/middleware/OBaseMiddlewareReader.sol
@@ -678,19 +678,23 @@ contract OBaseMiddlewareReader is
         for (uint256 i; i < operatorsLength_;) {
             address operator = operators[i];
             bytes32 key = abi.decode(getOperatorKeyAt(operator, epochStartTs), (bytes32));
-            uint256 operatorPowerCached = getOperatorToPower(epoch, key);
-
-            if (operatorPowerCached != 0) {
-                validatorSet[len++] = IMiddleware.ValidatorData(operatorPowerCached, key);
-            } else {
-                uint256 power = _optmizedGetOperatorPowerAt(epochStartTs, sharedVaults, subnetwork, operator);
-                if (key != bytes32(0) && power != 0) {
-                    validatorSet[len++] = IMiddleware.ValidatorData(power, key);
-                }
-            }
 
             unchecked {
                 ++i;
+            }
+
+            if (key != bytes32(0)) {
+                uint256 power = getOperatorToPower(epoch, key);
+                if (power == 0) {
+                    power = _optmizedGetOperatorPowerAt(epochStartTs, sharedVaults, subnetwork, operator);
+                }
+
+                if (power != 0) {
+                    validatorSet[len] = IMiddleware.ValidatorData(power, key);
+                    unchecked {
+                        ++len;
+                    }
+                }
             }
         }
 
